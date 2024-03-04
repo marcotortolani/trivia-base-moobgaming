53f39085aff5b645df851333c3532543c733afeb
Marco Tortolani <mjtorto@gmail.com>
2024-03-04 11:24:13 -0300

    deleting PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
@@ -13,36 +12,5 @@ export default defineConfig({
   base: "./",
   plugins: [
     preact(),
-    VitePWA({
-      injectRegister: "script",
-      registerType: "autoUpdate",
-      devOptions: { enabled: false },
-      includeAssets: ["/img/logo.webp"],
-      manifest: {
-        name: "Trivia Club Media Fest by MediaMoob",
-        short_name: "Trivia ClubMediaFest",
-        lang: "es",
-        description: "Trivia Club Media Fest - Juego de preguntas con categorías - by Media Moob",
-        theme_color: "#C46253",
-        icons: [
-          {
-            src: "/img/logo.webp",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/img/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "/img/logo-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-    }),
   ],
 });
