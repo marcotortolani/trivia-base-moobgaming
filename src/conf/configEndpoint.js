52c919d33fa592e26e975d4b4ea445786dca604c
Marco Tortolani <mjtorto@gmail.com>
2024-03-04 15:44:10 -0300

    change endpoint to github

--- src/conf/configEndpoint.js
+++ src/conf/configEndpoint.js
@@ -1,4 +1,4 @@
-// export const configEndpoint =
-//   'https://raw.githubusercontent.com/marcotortolani/trivia-base-moobgaming/main/src/conf/config.json';
+export const configEndpoint =
+  'https://raw.githubusercontent.com/marcotortolani/trivia-base-moobgaming/main/src/conf/config.json';
 
-export const configEndpoint = 'http://test.api.gaming.moob.club/api/v1/getTrivia';
+// export const configEndpoint = 'http://test.api.gaming.moob.club/api/v1/getTrivia';
