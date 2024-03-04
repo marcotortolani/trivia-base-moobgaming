2599a08c50ae3cc6da9f0fde0930f0f9ddc12403
Marco Tortolani <mjtorto@gmail.com>
2024-03-04 17:04:11 -0300

    adding hash param & github endpoint

--- src/conf/configEndpoint.js
+++ src/conf/configEndpoint.js
@@ -1,4 +1,7 @@
+// export const configEndpoint =
+//   'https://raw.githubusercontent.com/marcotortolani/trivia-base-moobgaming/main/src/conf/config.json';
+
 export const configEndpoint =
-  'https://raw.githubusercontent.com/marcotortolani/trivia-base-moobgaming/main/src/conf/config.json';
+  'https://raw.githubusercontent.com/marcotortolani/config-trivia-base/main/conf/config.json';
 
 // export const configEndpoint = 'http://test.api.gaming.moob.club/api/v1/getTrivia';
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,22 @@
 import { render } from 'preact';
 import { HashRouter } from 'react-router-dom';
 import { useDataConfig } from './helpers/useDataConfig';
-import { configEndpoint } from "./conf/configEndpoint";
+import { configEndpoint } from './conf/configEndpoint';
 import { App } from './app';
 import { ConfigProvider } from './ConfigProvider';
 import { LoadingTrivia } from './components/LoadingTrivia';
 import './index.css';
 
+const hash01 = 'c4ca4238a0b923820dcc509a6f75849b';
+const hash02 = 'c81e728d9d4c2f636f067f89cc14862c';
+
+const urlParams = new URLSearchParams(window.location.search);
+const hash = urlParams.get('hash');
+
 function Application() {
+  console.log('hash: ', hash);
   const dataConfig = useDataConfig(configEndpoint);
+  //const dataConfig = useDataConfig(configEndpoint + `/${hash01}`);
 
   return (
     <HashRouter>
