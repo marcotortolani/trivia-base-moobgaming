171e4040a081d394e5b7a0d289207ceb8beefcfa
Marco Tortolani <mjtorto@gmail.com>
2024-07-30 11:55:26 -0300

    change name encrypt

--- src/services/encryptData.js
+++ src/services/encryptData.js
@@ -1,10 +1,10 @@
 import rc4Min from 'rc4.js'
 
-const test = new rc4Min('MoobgamingAJM')
+const rc4 = new rc4Min('MoobgamingAJM')
 
 export function encryptData({ gameHash, userHash, partialScore }) {
-  const dataEncrypted = test.encrypt(gameHash + userHash + partialScore)
-  const transactionID = test.encrypt(
+  const dataEncrypted = rc4.encrypt(gameHash + userHash + partialScore)
+  const transactionID = rc4.encrypt(
     Math.floor(10000 + Math.random() * 90000).toString()
   )
   const validationNumber = sumCharacters(dataEncrypted + transactionID)
