8454c5fd7e91889f9cda6732adbb1cc39a5a83f5
Marco Tortolani <mjtorto@gmail.com>
2024-08-14 18:02:55 -0300

    update dependencies to fix lottie error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from "vite";
-import preact from "@preact/preset-vite";
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import filterReplace from 'vite-plugin-filter-replace';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +8,20 @@ export default defineConfig({
     chunkSizeWarningLimit: 1600,
   },
   server: {
-    host: true
+    host: true,
   },
-  base: "./",
+  base: './',
   plugins: [
     preact(),
+    // Workaround warning with lottie - https://github.com/airbnb/lottie-web/issues/2927
+    filterReplace([
+      {
+        filter: ['node_modules/lottie-web/build/player/lottie.js'],
+        replace: {
+          from: "eval('[function _expression_function(){' + val + ';scoped_bm_rt=$bm_rt}]')[0]",
+          to: "(new Function('scoped_bm_rt', val + ';return $bm_rt;'))()",
+        },
+      },
+    ]),
   ],
-});
+})
